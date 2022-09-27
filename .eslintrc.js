6e31581140bb15039895d503df869b34596ab7ff
LightXJ <2395461472@qq.com>
2022-09-27 18:02:04 +0800

    init: first react

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "plugins": ["react"],
+  "extends": "eslint:recommended",
+  "parser": "@babel/eslint-parser",
+  "env":{
+    "browser": true,
+    "node": true,
+    "es6": true
+  }
+}
