39003d6944156957210655a8a831d6ee385d6a43
LightXJ <2395461472@qq.com>
2022-09-27 19:27:53 +0800

    feat: concurrent mode

--- src/index.js
+++ src/index.js
@@ -24,22 +24,90 @@ const createElement = (type, props, ...children)=>{
   }
 }
 
-const render = (element, container)=>{
-  const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode(""):document.createElement(element.type);
+function createDom(fiber){
+  const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type);
 
   const isProperty = key => key !== 'children';
 
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name]
-    })
-
-  element.props.children.forEach(child=>{
-    render(child, dom);
+  Object.keys(fiber.props)
+  .filter(isProperty)
+  .forEach(name => {
+    dom[name] = fiber.props[name]
   })
 
-  container.appendChild(dom);
+  return dom;
+}
+
+let nextUnitOfWork = null;
+const render = (element, container)=>{
+
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+
+  function performUnitOfWork(fiber){
+    // TODO add dom node
+    if( !fiber.dom){
+      fiber.dom = createDom(fiber);
+    }
+
+    if(fiber.parent){
+      fiber.parent.dom.appendChild(fiber.dom)
+    }
+
+    // TODO create new fibers
+    const elements = fiber.props.children;
+    let index = 0;
+    let prevSibling = null;
+
+    while(index < elements.length){
+      const element = elements[index];
+
+      const newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: fiber,
+        dom: null,
+      }
+
+      if(index === 0){
+        fiber.child = newFiber;
+      }else{
+        prevSibling.sibling = newFiber;
+      }
+
+      prevSibling = newFiber;
+      index ++;
+    }
+
+    // TODO return next unit of work
+    if(fiber.child){
+      return fiber.child
+    }
+    let nextFiber = fiber;
+    while(nextFiber){
+      if(nextFiber.sibling){
+        return nextFiber.sibling;
+      }
+      nextFiber = nextFiber.parent;
+    }
+    
+  }
+
+  function workLoop(deadline){
+    let shouldYield = false;
+    while( nextUnitOfWork && !shouldYield){
+      nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+      console.log('working nextUnitOfWork', nextUnitOfWork);
+      shouldYield = deadline.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop);
+  }
+
+  requestIdleCallback(workLoop);
 }
 
 const Didact = {
@@ -48,7 +116,7 @@ const Didact = {
 }
 
 const element = (
-  <div id="foo">
+  <div id="foo" className="red">
     <a>bar</a>
     <b />
   </div>
